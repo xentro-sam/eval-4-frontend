a6c6f28b4a5f63b310efaef16cb16de401302517
Shivam Agarwal <itisshivam22@gmail.com>
2023-03-09 22:48:29 +0530

    feat: fetch content types and display them

--- src/components/ContentTypeContainer/index.jsx
+++ src/components/ContentTypeContainer/index.jsx
@@ -1,7 +1,16 @@
 import * as React from 'react';
 import './ContentTypeContainer.css';
+import makeRequest from '../../utils/makeRequest';
+import {GET_CONTENT_TYPES} from '../../constants/apiEndPoints';
 
 export default function ContentTypeContainer() {
+  const [contentTypes, setContentTypes] = React.useState([]);
+  React.useEffect(() => {
+    makeRequest(GET_CONTENT_TYPES, {})
+        .then((response) => {
+          setContentTypes(response);
+        });
+  }, []);
   return (
     <div id="content-type-container">
       <div id="content-type-container-search">
@@ -10,6 +19,15 @@ export default function ContentTypeContainer() {
       <div id="content-type-container-content">
         <div id="add-content-type-button">
             + New Type
+        </div>
+        <div id="content-type-container-content-list">
+          {contentTypes.map((contentType) => {
+            return (
+              <div className="content-type-container-content-list-item" key={contentType.id}>
+                {contentType.contentTypeName}
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
---
+++ src/constants/apiEndPoints.js
@@ -0,0 +1,6 @@
+export const BACKEND_URL = 'http://localhost:8000';
+
+export const GET_CONTENT_TYPES = {
+  url: '/contentTypes',
+  method: 'get',
+};
