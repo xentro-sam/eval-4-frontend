831263ac3231d8b54c1e8b88768120847f8c3c73
Shivam Agarwal <itisshivam22@gmail.com>
2023-03-10 07:04:49 +0530

    feat: add basic navigations

--- src/components/CollectionSidebar/index.jsx
+++ src/components/CollectionSidebar/index.jsx
@@ -1,8 +1,18 @@
 import * as React from 'react';
 import './CollectionSidebar.css';
 import PropTypes from 'prop-types';
+import {useNavigate} from 'react-router';
+import {CONTENT_TYPE_BUILDER, CONTENT_TYPE} from '../../constants/routes';
 
 export default function CollectionSidebar(props) {
+  const navigate = useNavigate();
+  const navigateToContentBuilder = () => {
+    navigate(CONTENT_TYPE_BUILDER);
+  };
+  const navigateToContentType = (id) => {
+    const url = CONTENT_TYPE.replace(':contentTypeId', id);
+    navigate(url);
+  };
   return (
     <div id="collection-sidebar">
       <div id="sidebar-header">
@@ -17,14 +27,14 @@ export default function CollectionSidebar(props) {
             <ul>
               {props.collectionTypes.map((collectionType) => {
                 return (
-                  <li key={collectionType.id} id="collection-type-item">{collectionType.contentTypeName}</li>
+                  <li key={collectionType.id} id="collection-type-item" onClick={() => navigateToContentType(collectionType.id)}>{collectionType.contentTypeName}</li>
                 );
               })}
             </ul>
           </div>
         </div>
       </div>
-      <div id="content-type-builder">
+      <div id="content-type-builder" onClick={navigateToContentBuilder}>
             Content Type Builder
       </div>
     </div>
--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,2 +1,3 @@
 export const LANDING = '/';
+export const CONTENT_TYPE_BUILDER = '/';
 export const CONTENT_TYPE = '/:contentTypeId';
